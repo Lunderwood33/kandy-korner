59b24f39f3347402d815c4ab44e6bec684b863f8
Lunderwood33 <donnie.underwood.11@gmail.com>
2021-09-02 13:46:57 -0500

    initial commit

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,16 @@
+export const KandyKorner = () => {
+    return (
+       <>
+        <h1>Welcome to Kandy Korner</h1>
+
+       <h2>Kandy</h2>
+       
+
+       <h2>More Kandy</h2>
+       
+        
+       <h2>Even more Kandy</h2>
+       
+        </>
+    )
+}
